6e97a607ba698c3169ffaada680baca738dce339
Carlos R. Zúñiga <carlosrozuma@gmail.com>
2018-09-14 17:39:37 -0700

    Usage like script available

--- Encryptor.php
+++ Encryptor.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "Sodium.php";
-$crypt = new Sodium();
+error_reporting( E_ERROR );
 
 
 //$message = "Hola soy un mensaje muy loco";
@@ -23,8 +23,52 @@ $crypt = new Sodium();
 //echo $crypt->decrypt($res);
 //echo "<br><br>";
 
-//$crypt->newLogin("carlos", "123");
-if( $crypt->validate( "carlos", "12543" ) )
-    echo "Success";
-else
-    echo "Fail";
+
+$action = $argv[1];
+$user   = $argv[2];
+$secret = $argv[3];
+
+if( empty($action) )
+    echo "Action Empty";
+
+
+try{
+    $crypt = new Sodium();
+
+    //when a new keys are crated are required new access credentials too
+    if( $action === 'genkeys' ){
+        echo $crypt->generateKeypair();
+    }
+
+    //New keys generation
+    else if( $action === 'new_access' ){
+        if( empty($user) )
+            echo "User Empty";
+        if( empty($secret) )
+            echo "Secret Empty";
+
+        $crypt->newLogin($user, $secret);
+        echo "Access credentials changed";
+    }
+
+    //User decrypt validation
+    else if( $action === 'validate' ){
+        if( empty($user) )
+            echo "User Empty";
+        if( empty($secret) )
+            echo "Secret Empty";
+
+
+        if( $crypt->validate( $user, $secret ) )
+            echo "Success";
+        else
+            echo "Fail";
+    }
+    else
+        echo "$action is an invalid action";
+
+} catch (Exception $e) {
+    echo "Error: ".$e->getMessage();
+}
+
+
